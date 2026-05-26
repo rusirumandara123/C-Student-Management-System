a8d9cf38189add16edf8a65cd525eceb3555f53c
rusirumandara123 <rusirumandara123@gmail.com>
2026-05-27 00:25:36 +0530

    Data save file create

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,19 @@
+
+        cout<<"Add student"<<endl;
+        break;
+
+        case 2:
+        cout<<"View Student"<<endl;
+        break;
+
+        case 3:
+        cout<<"Search Student"<<endl;
+        break;
+
+        case 4:
+        cout<<"Exit"<<endl;
+    }
+
+
+    return 0;
+}
